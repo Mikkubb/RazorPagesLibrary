8b44ac5ff415594da44270cc5cde69d7c78fde22
Jakub Michalski <Jakub.Michalski@microsoft.wsei.edu.pl>
2023-06-10 13:11:05 +0200

    Update Book.cs

--- RazorPagesLibrary/Models/Book.cs
+++ RazorPagesLibrary/Models/Book.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesLibrary.Models
 {
@@ -8,6 +9,9 @@ namespace RazorPagesLibrary.Models
         public string? Title { get; set; }
         public string? Author { get; set; }
         public string? Genre { get; set; }
+
+        [Display(Name = "Price (Euro)")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 
