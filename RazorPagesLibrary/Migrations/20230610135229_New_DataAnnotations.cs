e4bc6edaf5c3d717bd3ad8b123d8308fa70c8162
Jakub Michalski <Jakub.Michalski@microsoft.wsei.edu.pl>
2023-06-10 15:54:25 +0200

    Add Validation

---
+++ RazorPagesLibrary/Migrations/20230610135229_New_DataAnnotations.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class New_DataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Book",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
--- RazorPagesLibrary/Models/Book.cs
+++ RazorPagesLibrary/Models/Book.cs
@@ -6,15 +6,30 @@ namespace RazorPagesLibrary.Models
     public class Book
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Title { get; set; } = string.Empty;
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Author { get; set; } = string.Empty;
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Genre { get; set; } = string.Empty;
 
-        [Display(Name = "Price (Euro)")]
+        [Display(Name = "Price")]
         [Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
         [Display(Name = "Rating (0-10)")]
+        [Column(TypeName = "int")]
+        [Range(0, 10)]
+        [Required]
         public int Rating { get; set; }
     }
 
